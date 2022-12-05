5f57edd3f7dc61de5eb9a0558362d0db3ba8aed4
Júlia Lamboglia <julia.conip@gmail.com>
2022-12-05 17:52:28 -0300

    #1 alterado title do MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Conifer's Portfolio",
       debugShowCheckedModeBanner: false,
       theme: getDefaultThemeData(),
       home: const MainScreen(),
